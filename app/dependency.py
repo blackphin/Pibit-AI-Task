7a5791a8c77889d48a722e62fe08f95cf007c44f
Shivansh Goel <shivanshgoel009@gmail.com>
2024-05-18 02:19:33 +0530

    Added setup_logger functionality

--- app/dependency.py
+++ app/dependency.py
@@ -6,66 +6,67 @@ import logging
 config = configparser.ConfigParser()
 config.read('error-config.ini')
 
-format = config.get('logging_format', 'format', raw=True)
-
-for error_level in config['error_levels']:
-    level_name = error_level
-    level_num = int(config['error_levels'][error_level])
-    method_name = level_name.lower()
+fmt = config.get('logging_format', 'format', raw=True)
 
 
-# levelName = "NEW"
-# levelNum = 15
-# methodName = levelName.lower()
-# fmt = '{"level": "%(levelname)s", "log_string": "%(message)s", "timestamp": "%(asctime)s", "metadata": {"source": "%(log_file)s"}}'
+class LogFilter(logging.Filter):
+    def __init__(self, log_file: str):
+        self.log_file = log_file
+
+    def filter(self, record):
+        record.log_file = self.log_file
+        return True
 
 
-# class AppFilter(logging.Filter):
-#     def __init__(self, log_file: str):
-#         self.log_file = log_file
+def setup_logger(logger_name, level=1):
+    l = logging.getLogger(logger_name)
 
-#     def filter(self, record):
-#         record.log_file = self.log_file
-#         return True
+    def logForLevel(message, *args, **kwargs):
+        l.log(level_num, message, *args, **kwargs)
+
+    def logToRoot(message, *args, **kwargs):
+        l.log(level_num, message, *args, **kwargs)
+
+    for error_level in config['error_levels']:
+        level_name = error_level
+        level_num = int(config['error_levels'][error_level])
+
+        method_name = level_name.lower()
+
+        # Add Logging Level
+        logging.addLevelName(level_num, level_name)
+        setattr(logging, level_name, level_num)
+        setattr(logging.getLoggerClass(), method_name, logForLevel)
+        setattr(logging, method_name, logToRoot)
+
+    # Add logging file name
+    l.addFilter(LogFilter(log_file=logger_name))
+
+    # Declare file and stream handlers
+    fileHandler = logging.FileHandler(
+        f'{logger_name}.log', mode='w')
+    formatter = logging.Formatter(fmt)
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    # Set Level and Handlers
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(streamHandler)
 
 
-# def setup_logger(logger_name, level=4):
-#     def logForLevel(message, *args, **kwargs):
-#         l.log(levelNum, message, *args, **kwargs)
+setup_logger('log1')
+setup_logger('log2')
+setup_logger('log3')
 
-#     def logToRoot(message, *args, **kwargs):
-#         l.log(levelNum, message, *args, **kwargs)
+log1 = logging.getLogger('log1')
+log2 = logging.getLogger('log2')
+log3 = logging.getLogger('log3')
 
-#     l = logging.getLogger(logger_name)
-
-#     logging.addLevelName(levelNum, levelName)
-#     setattr(logging, levelName, levelNum)
-#     setattr(logging.getLoggerClass(), methodName, logForLevel)
-#     setattr(logging, methodName, logToRoot)
-
-#     l.addFilter(AppFilter(log_file=logger_name))
-
-#     fileHandler = logging.FileHandler(
-#         fr'D:\OneDrive\Repositories\Pibit-AI-Task\{logger_name}.log', mode='w')
-#     formatter = logging.Formatter(fmt)
-#     fileHandler.setFormatter(formatter)
-#     streamHandler = logging.StreamHandler()
-#     streamHandler.setFormatter(formatter)
-
-#     l.setLevel(level)
-#     l.addHandler(fileHandler)
-#     l.addHandler(streamHandler)
-
-
-# setup_logger('log1')
-# setup_logger('log2')
-# setup_logger('log3')
-
-# log1 = logging.getLogger('log1')
-# log2 = logging.getLogger('log2')
-# log3 = logging.getLogger('log3')
-
-# log1.info('Info for log 1!')
-# log2.info('Info for log 2!')
-# log1.error('Oh, no! Something went wrong!')
-# log3.log(15, "Hello, World!")
+log1.info('Info for log 1!')
+log1.log(1, "Hello, World1!")
+log2.info('Info for log 2!')
+log2.log(2, "Hello, World2!")
+log3.info('Info for log 3!')
+log3.log(3, "Hello, World3!")
