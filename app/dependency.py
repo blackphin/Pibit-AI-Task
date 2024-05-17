85a4270f11899103f17be2110611b88f0faec8e5
Shivansh Goel <shivanshgoel009@gmail.com>
2024-05-18 01:28:17 +0530

    Added config file functionality

--- app/dependency.py
+++ app/dependency.py
@@ -4,70 +4,68 @@ import configparser
 import logging
 
 config = configparser.ConfigParser()
-config.read('error-config.INI')
+config.read('error-config.ini')
 
-print(config['DEFAULT']['path'])     # -> "/path/name/"
-config['DEFAULT']['path'] = '/var/shared/'    # update
-config['DEFAULT']['default_message'] = 'Hey! help me!!'   # create
+format = config.get('logging_format', 'format', raw=True)
 
-with open('FILE.INI', 'w') as configfile:    # save
-    config.write(configfile)
-
-
-levelName = "NEW"
-levelNum = 15
-methodName = levelName.lower()
-fmt = '{"level": "%(levelname)s", "log_string": "%(message)s", "timestamp": "%(asctime)s", "metadata": {"source": "%(log_file)s"}}'
+for error_level in config['error_levels']:
+    level_name = error_level
+    level_num = int(config['error_levels'][error_level])
+    method_name = level_name.lower()
 
 
-class AppFilter(logging.Filter):
-    def __init__(self, log_file: str):
-        self.log_file = log_file
-
-    def filter(self, record):
-        record.log_file = self.log_file
-        return True
+# levelName = "NEW"
+# levelNum = 15
+# methodName = levelName.lower()
+# fmt = '{"level": "%(levelname)s", "log_string": "%(message)s", "timestamp": "%(asctime)s", "metadata": {"source": "%(log_file)s"}}'
 
 
-def setup_logger(logger_name, level=4):
-    def logForLevel(message, *args, **kwargs):
-        l.log(levelNum, message, *args, **kwargs)
+# class AppFilter(logging.Filter):
+#     def __init__(self, log_file: str):
+#         self.log_file = log_file
 
-    def logToRoot(message, *args, **kwargs):
-        l.log(levelNum, message, *args, **kwargs)
-
-    l = logging.getLogger(logger_name)
-
-    logging.addLevelName(levelNum, levelName)
-    setattr(logging, levelName, levelNum)
-    setattr(logging.getLoggerClass(), methodName, logForLevel)
-    setattr(logging, methodName, logToRoot)
-
-    l.addFilter(AppFilter(log_file=logger_name))
-
-    fileHandler = logging.FileHandler(
-        fr'D:\OneDrive\Repositories\Pibit-AI-Task\{logger_name}.log', mode='w')
-    formatter = logging.Formatter(fmt)
-    fileHandler.setFormatter(formatter)
-    streamHandler = logging.StreamHandler()
-    streamHandler.setFormatter(formatter)
-
-    l.setLevel(level)
-    l.addHandler(fileHandler)
-    l.addHandler(streamHandler)
-
-    # l = logging.LoggerAdapter(l, extra)
+#     def filter(self, record):
+#         record.log_file = self.log_file
+#         return True
 
 
-setup_logger('log1')
-setup_logger('log2')
-setup_logger('log3')
+# def setup_logger(logger_name, level=4):
+#     def logForLevel(message, *args, **kwargs):
+#         l.log(levelNum, message, *args, **kwargs)
 
-log1 = logging.getLogger('log1')
-log2 = logging.getLogger('log2')
-log3 = logging.getLogger('log3')
+#     def logToRoot(message, *args, **kwargs):
+#         l.log(levelNum, message, *args, **kwargs)
 
-log1.info('Info for log 1!')
-log2.info('Info for log 2!')
-log1.error('Oh, no! Something went wrong!')
-log3.log(15, "Hello, World!")
+#     l = logging.getLogger(logger_name)
+
+#     logging.addLevelName(levelNum, levelName)
+#     setattr(logging, levelName, levelNum)
+#     setattr(logging.getLoggerClass(), methodName, logForLevel)
+#     setattr(logging, methodName, logToRoot)
+
+#     l.addFilter(AppFilter(log_file=logger_name))
+
+#     fileHandler = logging.FileHandler(
+#         fr'D:\OneDrive\Repositories\Pibit-AI-Task\{logger_name}.log', mode='w')
+#     formatter = logging.Formatter(fmt)
+#     fileHandler.setFormatter(formatter)
+#     streamHandler = logging.StreamHandler()
+#     streamHandler.setFormatter(formatter)
+
+#     l.setLevel(level)
+#     l.addHandler(fileHandler)
+#     l.addHandler(streamHandler)
+
+
+# setup_logger('log1')
+# setup_logger('log2')
+# setup_logger('log3')
+
+# log1 = logging.getLogger('log1')
+# log2 = logging.getLogger('log2')
+# log3 = logging.getLogger('log3')
+
+# log1.info('Info for log 1!')
+# log2.info('Info for log 2!')
+# log1.error('Oh, no! Something went wrong!')
+# log3.log(15, "Hello, World!")
