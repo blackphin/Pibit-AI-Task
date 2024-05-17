ab93d691a343107b863cffd3b57c145938ecb88c
Shivansh Goel <shivanshgoel009@gmail.com>
2024-05-18 04:07:03 +0530

    Added query interface services

--- app/routers/ingestor/ingestor.py
+++ app/routers/ingestor/ingestor.py
@@ -5,7 +5,8 @@ from fastapi import APIRouter, Depends, HTTPException, Request, Security, status
 from routers.ingestor import schemas
 
 # Services Imports
-from routers.ingestor import services
+from routers.ingestor import services as ingestor_services
+from routers.query import services as query_services
 
 router = APIRouter(tags=['Error Ingestor'], prefix='/ingestor')
 
@@ -20,7 +21,9 @@ def current_status():
 @router.post("/error/{api_name}", status_code=status.HTTP_201_CREATED)
 def error_ingestor(api_name: str, payload: schemas.IngestorSchema):
     try:
-        services.log_error(api_name, payload.message, payload.level)
+        ingestor_services.log_error(api_name, payload.message, payload.level)
+        query_services.update_error_log(
+            ingestor_services.get_log_path(api_name) + ".log")
         return {"status": "Error Ingested!"}
     except Exception as e:
         return {"status": e}
